612bb98afb16310a362c799e3e644e78bf56b632
MayowaOladosu <20852939-MayowaOladosu@users.noreply.replit.com>
2024-10-02 08:48:55 +0000

    Agent query: What is the debug output after restarting the Flask application? Has the MongoDB connection been established successfully?

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
-    MONGO_URI = 'mongodb+srv://mayowa:semmat@cluster0.mongodb.net/first?retryWrites=true&w=majority'
+    MONGO_URI = 'mongodb+srv://mayowa:semmat@cluster0.9a3xw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
